23bdabaddc141f3377d7515c91c462e9d366c9d0
Yohan_Garcia <75887329+yohanJS@users.noreply.github.com>
2022-11-16 08:24:54 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.CurrentTime = DateTime.Now;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Edit() 
+        {
+            return View();
+        }
+    }
+}
