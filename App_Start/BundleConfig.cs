4a656a3a229c63bf09356cb45439abaa84d8c31d
Yohan_Garcia <75887329+yohanJS@users.noreply.github.com>
2022-12-17 16:02:55 -0500

    bootstrap theme added

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace MvcMusicStore
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-paper.css",
                       "~/Content/site.css"));
         }
     }
